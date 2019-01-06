35200274ca681396be2d46323be0c4af80e1be19
Shreedhar Prabhu <32056192+shree1497@users.noreply.github.com>
2019-01-06 19:28:18 +0530

    Add files via upload

---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+#import <fluttery_audio/FlutteryAudioPlugin.h>
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FlutteryAudioPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutteryAudioPlugin"]];
+}
+
+@end
